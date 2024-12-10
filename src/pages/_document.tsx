c13e72c7b6e9e6a1f5e29c8c523f94e5cfc9b01a
Gaurav <95956977+gduke-k@users.noreply.github.com>
2024-12-10 16:14:57 +0530

    Update _document.tsx

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,14 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        {/* Basic SEO Meta Tags */}
+        <meta charSet="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta name="description" content="Weframetech" />
+        <meta name="keywords" content="tech news, product reviews, AI trends, gadgets, Weframetech, software" />
+        <meta name="author" content="Gaurav" />
+      </Head>
       <body className="antialiased">
         <Main />
         <NextScript />
