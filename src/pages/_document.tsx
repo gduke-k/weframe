2f84a3e23a9464b62f33da3e468c5eda7eb882e0
Gaurav <95956977+gduke-k@users.noreply.github.com>
2024-12-10 16:28:38 +0530

    Update _document.tsx

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -8,6 +8,7 @@ export default function Document() {
         <meta charSet="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <meta name="description" content="Weframetech" />
+        <title>WeFrame Tech</title>
         <meta name="keywords" content="tech news, product reviews, AI trends, gadgets, Weframetech, software" />
         <meta name="author" content="Gaurav" />
       </Head>
